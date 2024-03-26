3e8b4e8878bc8c70b3a2843d77227e18218b17d6
shehabhussien <140269202+shehabhussien@users.noreply.github.com>
2024-03-27 00:15:48 +0200

    Add files via upload

---
+++ 7th_Submission_bonus.ino
@@ -0,0 +1,122 @@
+// Sensors
+#define left 6
+#define center 7
+#define right 8
+
+//motor one
+#define ENA 9
+#define IN1 2
+#define IN2 3
+
+//motor two
+#define ENB 10
+#define IN3 4
+#define IN4 5
+
+//Ultra sonic
+const int trigPin = 12;
+const int echoPin = 11;
+//LAMBA
+int lamp=13;
+
+int Speed = 105;
+int Speeds = 170;
+int Speedlaf=85;
+void setup() {
+  Serial.begin(9600);
+  pinMode(left, INPUT);
+  pinMode(center, INPUT);
+  pinMode(right, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(lamp, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+  bool leftV = digitalRead(left);
+  bool centerV = digitalRead(center);
+  bool rightV = digitalRead(right);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  Serial.println(rightV);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  if (distance >= 30) {
+   if (leftV == 1 && centerV == 0 && rightV == 1) {
+    carforward();
+    Serial.println("forward");
+  } else if (leftV == 0 && centerV == 0 && rightV == 0) {
+    carStop();
+    digitalWrite(lamp, HIGH);
+  } else if (leftV == 1 && centerV == 1 && rightV == 1) {
+    carStop();
+    digitalWrite(lamp, HIGH);
+  } else if (leftV == 0 && centerV == 0 && rightV == 1) {
+    carturnleft();
+    digitalWrite(lamp, LOW);
+  } else if (leftV == 1 && centerV == 0 && rightV == 0) {
+    carturnright();
+    digitalWrite(lamp, LOW);
+  } else if (leftV == 0 && centerV == 1 && rightV == 1) {
+    carturnleft();
+    digitalWrite(lamp, LOW);
+  } else if (leftV == 1 && centerV == 1 && rightV == 0) {
+    carturnright();
+    digitalWrite(lamp, LOW);
+  }
+}
+else{   
+    analogWrite(ENB, 0);
+    analogWrite(ENA, 0);
+    digitalWrite(lamp, HIGH);
+  }
+  delay(20);
+}
+void carforward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carturnleft() {
+  analogWrite(ENA, Speeds);
+  analogWrite(ENB, Speedlaf);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carturnright() {
+  analogWrite(ENA, Speeds);
+  analogWrite(ENB, Speedlaf);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carlafalafa() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carStop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
